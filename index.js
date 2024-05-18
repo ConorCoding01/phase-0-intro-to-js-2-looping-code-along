8193ac5bf15db67c2f7ddfd3e9f537259f35a2fb
Conor <brendanlane8901@gmail.com>
2024-05-17 22:06:32 -0500

    JSCountDown

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(names) {
+
+    let thankYouMessages = [];
+    
+    for (let i = 0; i < names.length; i++) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+
+    return thankYouMessages;
+}
+
+
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
+
+countDown(10);
